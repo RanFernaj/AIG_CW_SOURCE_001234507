eedf1c52241676950a26d06c13fa3299f268b444
Ranen <rf4958o@gre.ac.uk>
2024-11-27 17:56:15 +0000

    Player Kills changed
    Found workaround for player kills

--- AIG_CW_SOURCE_001234507/Assets/Scripts/Damage.cs
+++ AIG_CW_SOURCE_001234507/Assets/Scripts/Damage.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public float health = 50f;
-    public GameDirector gameDirector;
+    //public GameDirector gameDirector;
 
     public void TakeDamage(float amount) 
     {
         health -= amount;
         if (health <= 0)
         {
+            GameDirector gameDirector = FindObjectOfType<GameDirector>();
             gameDirector.AddPlayerKills(1);
             Die();
         }
