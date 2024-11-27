fae6e46d95f18fc0d3aee6303368c453a66c4523
Ranen Fernandes <124270484+RanFernaj@users.noreply.github.com>
2024-11-27 11:52:10 +0000

    Changed git ignore and game director file

--- AIG_CW_SOURCE_001234507/Assets/Scripts/GameDirector.cs
+++ AIG_CW_SOURCE_001234507/Assets/Scripts/GameDirector.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class GameDirector : MonoBehaviour
 {
+    [Header("Game Object References")]
+    public GameObject player;
+    public GameObject enemy;
+
+    [Header("Values")]
+    public int kills;
+    public int currentEnemies;
+    public enum Difficulties 
+    {
+        EASY, 
+        MEDIUM, 
+        HARD
+    };
+    public Difficulties currentDifficulty;
+
+    // Have different fucntions for each difficulty 
+    // For each difficulty ther should be a min and max value
+    // Probabilty caluclation should also occur here 
+    // When a value for a gameobject ie. Enemy or spawner 
+    // 
+
+
     // Start is called before the first frame update
     void Start()
     {
